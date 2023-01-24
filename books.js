14879966885c53e2a62480fa8dd5e896bf937b28
Herry333 <98563053+Herry333@users.noreply.github.com>
2023-01-24 14:43:00 +0500

    Update books.js

--- books.js
+++ books.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const books = require('./books.json');
 
-// Get all the books
+
 router.get('/', (req, res) => {
   res.json(books);
 });
 
-// Get a specific book
+
 router.get('/:id', (req, res) => {
   const { id } = req.params;
   res.json(books.filter((ele) => ele.id === parseInt(id)));
